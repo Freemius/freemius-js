57bca1a4038e7d1925fd842d1a9f3fad0ddc9aa3
Swashata Ghosh <swas@freemius.com>
2025-09-15 19:49:35 +0530

    [api] Send API version and User Agent

--- packages/sdk/src/api/client.ts
+++ packages/sdk/src/api/client.ts
@@ -1,13 +1,30 @@
-import createClient from 'openapi-fetch';
+import createClient, { Middleware } from 'openapi-fetch';
 import type { paths } from './schema';
+import { version } from '../../package.json';
+
+const middleware: Middleware = {
+    async onRequest({ request }) {
+        const originalUrl = new URL(request.url);
+
+        // Add a `sdk_version` query parameter to every request
+        originalUrl.searchParams.set('sdk_version', version);
+
+        return new Request(originalUrl, request);
+    },
+};
 
 export function createApiClient(baseUrl: string, bearerToken?: string) {
-    return createClient<paths>({
+    const client = createClient<paths>({
         baseUrl,
         headers: {
             Authorization: bearerToken ? `Bearer ${bearerToken}` : undefined,
+            'User-Agent': `Freemius/JS-SDK`,
         },
     });
+
+    client.use(middleware);
+
+    return client;
 }
 
 export type FsApiClient = ReturnType<typeof createApiClient>;
